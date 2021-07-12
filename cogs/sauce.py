2270c61f793f9b79a41a0c3fc9588e58b1f35c26
Vein05 <72024472+Vein05@users.noreply.github.com>
2021-07-12 19:11:18 +0545

    changed things, and added support for anime info

--- cogs/sauce.py
+++ cogs/sauce.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import random
-from saucenao_api import SauceNao, VideoSauce
+from pysaucenao import DailyLimitReachedException, AnimeSource,UnknownStatusCodeException ,GenericSource, InvalidImageException, InvalidOrWrongApiKeyException, MangaSource, SauceNao, SauceNaoException, ShortLimitReachedException, VideoSource
+
 from disputils import BotEmbedPaginator
 import re
 from urllib import parse
@@ -9,6 +10,7 @@ from urllib import parse
 class sauce(commands.Cog, name="Sauce"):
     def __init__(self, Bot):
         self.Bot = Bot
+        self.saucenao_keys = self.Bot.saucenao_keys
 
     @commands.command(aliases = ['saucenao'], description = "Let's you search for sauce with saucenao, works with image links. You also have the option to just use dh sauce and the bot will auto search the previous 10 messages and give sauce for the first image it finds. You also have the option to attach an image while using the command.", usage="dh sauce image.jpg \n dh sauce")
     @commands.guild_only()
@@ -35,37 +37,48 @@ class sauce(commands.Cog, name="Sauce"):
         if url == None:
             return await ctx.send("No image urls found in the last 10 messages please retry by uploading one.")
         try:
-            sauce = SauceNao(api_key = "2e2bd7105762f95bf1fdd688b2bbe34e28088873")
-            results = sauce.from_url(url)
+            google_url  = f"https://www.google.com/searchbyimage?image_url={url}&safe=off"
+            yandex_url  = f"https://yandex.com/images/search?url={url}&rpt=imageview"
+            
+            sauce = SauceNao(api_key = random.choice(self.saucenao_keys), results_limit = 6)
+            embed = discord.Embed()
+            results = await sauce.from_url(url)
             thumbnail = results[0].thumbnail if (results[0].thumbnail != None) else "None"
             similarity = results[0].similarity if (results[0].similarity != None) else "None"
+            
+            if isinstance(results[0], AnimeSource):
+                 await results[0].load_ids()
+
             try:
                 source = results[0].urls[0]
-            except:
+            except IndexError:
                 source = "https://saucenao.com/search.php?url={}".format(parse.quote_plus(url))
             try:
-                author = results[0].author
-            except:
+                author = results[0].author_name
+            except AttributeError:
                 author = "Not given"
             try:
                 title = results[0].title
-            except:
+            except AttributeError:
                 title = "No title"
             index_id = results[0].index_id if (results[0].index_id != None) else "None"
             index_name = results[0].index_name if (results[0].index_name != None) else "None"
             if 70 >  similarity:
                 return await ctx.send("Couldn't find any thing for the given query.")
 
-            embed = discord.Embed()
+            
             embed.description=f"Sucessfully found closest image(`{title}`) with the following information."
             embed.add_field(name="Similarity", value = similarity)
             embed.add_field(name="Author", value =author )
             embed.add_field(name="Source", value=source, inline=False)
             embed.add_field(name="Index", value=f"ID : `{index_id}` \nName : `{index_name}`")
+            embed.add_field(name="Others", value=f"[<:google:864001090172354610>]({google_url})  "
+                                                f"[<:yandex:864002609466572840>]({yandex_url})"
+                                                ,inline=False)
             embed.set_thumbnail(url = thumbnail)
-            embed.set_footer(text=f"Enjoy!")
             await ctx.send(embed=embed)
-        except:
+
+        except UnknownStatusCodeException:
             url = "https://saucenao.com/search.php?url={}".format(parse.quote_plus(url))
             em = discord.Embed(description =f"Sorry nothing found you can try [here]({url}) if you'd like.")
             em.set_footer(text="Also, make sure your url ends with an image format.")
