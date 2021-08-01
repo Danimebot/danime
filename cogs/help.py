303e348f980c3f3aa1b6d0de73f2ffbfac89b729
Vein05 <man359905@gmail.com>
2021-08-01 16:49:29 +0545

    Well that fixed it for the time being.

--- cogs/help.py
+++ cogs/help.py
@@ -223,7 +223,6 @@ class vein9(commands.Cog, name='Help'):
         boot_time = str(psutil.boot_time() / 100000000)
         boot_time_round = boot_time[:4]
 
-        owner = self.Bot.get_user(427436602403323905)
         embed = discord.Embed(timestamp=self.Bot.starttime ,
             color=0x26fcff,
             description="```asciidoc\n"
@@ -236,9 +235,9 @@ class vein9(commands.Cog, name='Help'):
             )
         embed.set_author(name=f"Danime's Information")
         embed.add_field(name=f"General ", inline=True, value=f"```asciidoc\nBoot Time: {boot_time_round}s\nUsers: {users}\nServer: {guilds}```\n")
-        embed.add_field(name=f"Bot", inline=True, value=f"```asciidoc\nDiscord.py: {discord.__version__}\nPython: 3.8.7\nDanime: 1.4\n```")
+        embed.add_field(name=f"Bot", inline=True, value=f"```asciidoc\nDiscord.py: {discord.__version__}\nPython: 3.8.7\nDanime: 2.2\n```")
         embed.add_field(name=f"System", inline=False, value=f"```asciidoc\nOS: {sys.platform}\nCPU Usage: {cpu}%\n```")
-        embed.add_field(name=f"Creator", inline=False, value=f"```asciidoc\nUsername: {owner.name}#{owner.discriminator} [{owner.id}]```")
+        embed.add_field(name=f"Creator", inline=False, value=f"```asciidoc\nUsername: Vein#8177 [427436602403323905]```")
         embed.add_field(name=f"Links", inline=False, value=f'[Invite]({self.Bot.invite}) |  [Support Server]({self.Bot.support}) |  [Github]({self.Bot.github}) |  [Website]({self.Bot.website_link}) |  [Vote]({vote})')
         embed.set_footer(text=f"Last restart")
 
