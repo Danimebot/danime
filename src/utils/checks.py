20b5e3e6eb59230418c0391dfce44dac6f766a73
Vein05 <72024472+Vein05@users.noreply.github.com>
2021-08-14 13:00:40 +0545

    Finally

--- src/utils/checks.py
+++ src/utils/checks.py
@@ -1,48 +1,63 @@
 from discord.ext import commands, tasks
 import discord
 import pymongo
+from pymongo import MongoClient
+import json 
+import os
 
-premium_guilds = []
 
-def is_premium_guild():
-    async def predictate(ctx):
-        if ctx.guild.id in premium_guilds:
-            return True
-        else:
-            await ctx.send("This command needs your server to have premium activated. `dh premium` for more info.")
-            return False
-    return commands.check(predictate)
+path = "/home/ubuntu/danime/src/configs.json"
+
+if not os.path.exists(path):
+    path = "/home/vein/Documents/danime/src/configs.json"
+
+with open(path) as jsonfile:
+    obj = json.load(jsonfile)
+    db1_token = obj['data']['db1']
+    db2_token = obj['data']['db2']
 
 
 class _checks(commands.Cog):
 
     def __init__(self, Bot):
         self.Bot = Bot
-        self.append_premium.start()
     
 
-    @tasks.loop(seconds=60)
-    async def append_premium(self):
-        await self.Bot.wait_until_ready()
-        db =  self.Bot.db1['AbodeDB']
-        collection = db['premium_guilds']
-        get_guilds = {
-            "$group" : {
-                "_id" : "$_id",
-                }
-        }
+premium_guilds = []
 
-        pipeline = [
-            get_guilds
-        ]
-        results = collection.aggregate(pipeline)
-        
-        for answer in results:
-            guild = answer['_id']
-            if guild not in self.Bot.premium_guilds:
-                self.Bot.premium_guilds.append(guild)
-                premium_guilds.append(guild)
-        
+@tasks.loop(seconds=60)
+async def append_premium():
+    db =  MongoClient(db1_token)['AbodeDB']
+    collection = db['premium_guilds']
+    get_guilds = {
+        "$group" : {
+            "_id" : "$_id",
+            }
+    }
+    pipeline = [
+        get_guilds
+    ]
+    results = collection.aggregate(pipeline)
+    for answer in results:
+        guild = answer['_id']
+        if guild not in premium_guilds:
+            premium_guilds.append(guild)
+    print(premium_guilds)
+
+def get_guilds():
+    return premium_guilds
+
+def is_premium_guild():
+    async def predictate(ctx):
+        if ctx.guild.id in get_guilds():
+            return True
+        else:
+            await ctx.send("This command needs your server to have premium activated. `dh premium` for more info.")
+            return False
+    return commands.check(predictate)   
+
+append_premium.start()
+
 def setup (Bot):
     Bot.add_cog(_checks(Bot))
     print("Checks cog is working.")
