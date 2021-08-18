3097f444d2c67d09ddedf38fdd5d467009885d7b
Vein05 <72024472+Vein05@users.noreply.github.com>
2021-08-18 17:11:06 +0545

    Added a way to search fro videos too

--- src/cogs/sauce.py
+++ src/cogs/sauce.py
@@ -13,11 +13,11 @@ class sauce(commands.Cog, name="Sauce"):
         self.Bot = Bot
         self.saucenao_keys = self.Bot.saucenao_keys
 
-    @commands.command(aliases = ['saucenao'], description = "Let's you search for sauce with saucenao, works with image links. You also have the option to just use dh sauce and the bot will auto search the previous 10 messages and give sauce for the first image it finds. You also have the option to attach an image while using the command.", usage="dh sauce image.jpg \n dh sauce")
+    @commands.command(aliases = ['saucenao'], description = "Let's you search for sauce with saucenao, works with image links. You also have the option to just use dh sauce and the bot will auto search the previous 10 messages and give sauce for the first image it finds. You also have the option to attach an image while using the command. Now works with .mp4 formats too.", usage="dh sauce image.jpg \n dh sauce")
     @commands.guild_only()
     @commands.cooldown(1, 15, commands.BucketType.user)
     async def sauce(self, ctx, url=None):
-        #https://github.com/FujiMakoto/pysaucenao Nice thing
+        #https://github.com/FujiMakoto/pysaucenao Pray to the lord and savior
         if url!= None:
             if self.is_url(url) != True:
                 return await ctx.send("Your image url doesn't seem to be accurate. An image url should look like `https://danbooru.donmai.us/data/original/a2/d0/a2d093a060757d36d8a9f03bcbfbcd82.jpg`.")
@@ -26,7 +26,7 @@ class sauce(commands.Cog, name="Sauce"):
             await ctx.send("No url found in command, checking for attachments.", delete_after = 5)
             try:
                 url = ctx.message.attachments[0].url
-            except:
+            except: 
                 url = None
         
         if url == None:
@@ -38,6 +38,8 @@ class sauce(commands.Cog, name="Sauce"):
                     break
         if url == None:
             return await ctx.send("No image urls found in the last 10 messages please retry by uploading one.")
+        if url.endswith(('.mp4', '.webm', '.mov')):
+            url = url.replace("cdn.discordapp.com", "media.discordapp.net") + "?format=jpeg"
         try:
             google_url  = f"https://www.google.com/searchbyimage?image_url={url}&safe=off"
             yandex_url  = f"https://yandex.com/images/search?url={url}&rpt=imageview"
@@ -91,11 +93,11 @@ class sauce(commands.Cog, name="Sauce"):
                                                 f"[SauceNao]({saucenao_url})"
                                                 ,inline=False)
             
-            
+            embed.set_footer(text=f"The sauce is found from sauce-nao.net.")
             await ctx.send(embed=embed)
 
         
-        except UnknownStatusCodeException:
+        except SauceNaoException:
             url = "https://saucenao.com/search.php?url={}".format(parse.quote_plus(url))
             em = discord.Embed(description =f"Sorry nothing found you can try [here]({url}) if you'd like.")
             em.set_footer(text="Also, make sure your url ends with an image format.")
@@ -108,7 +110,7 @@ class sauce(commands.Cog, name="Sauce"):
             return await ctx.send("Something went worng, it seems no results from your queries.")
     
     def is_url(self, message):
-        pattern =  re.compile(r"^https?://\S+(\.jpg|\.png|\.jpeg|\.webp]|\.gif)$")
+        pattern =  re.compile(r"^https?://\S+(\.jpg|\.png|\.jpeg|\.webp]|\.gif|\.mp4)$")
         if not pattern.match(message):
             return False
         return True
